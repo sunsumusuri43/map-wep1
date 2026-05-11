3bad5c3d4aa880722ad4a77b73c88954ebd76bc9
sunsumusuri43 <sunsumusuri43@gmail.com>
2026-05-11 11:52:42 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openKakaoMap() {
+
+    window.open(
+        "https://map.kakao.com/",
+        "_blank"
+    );
+
+}
+
+function openNaverMap() {
+
+    window.open(
+        "https://map.naver.com/",
+        "_blank"
+    );
+
+}
